d64802959182ad18967326f4bd6675f310894784
asale123 <asale@vols.utk.edu>
2022-04-30 22:23:10 -0400

    Update ui.R
    Changing the column name of the data set to make it easier to code. I did this because every time I tried to save it the "i" would change to a question mark so it wouldn't save correctly. Changing the training and holdout data. Also adding a multiple models to test which one best predicts best. Finally testing the model using RMSE to see which one predicts closest to the training data . The model didn't work when I tried the search and neural so that is why they are commented out.

--- ui.R
+++ ui.R
@@ -16,12 +16,14 @@ library(lubridate)
 library(fpp3)
 library(parallel)
 library(doParallel)
+library(dbplyr)
 
 
-CREDITDATA <- read.csv('credit.csv')
+CREDITDATA <- read.csv("credit.csv")
 
 CREDITDATA$Month <- 492:1
 CREDITDATA$Month <- yearmonth(CREDITDATA$Month)
+rename(CREDITDATA, credit_in_millions = ï..credit_in_millions)
 
 CREDIT_TS <- tsibble(CREDITDATA, index = Month)
 CREDIT_TS %>% 
@@ -38,7 +40,29 @@ gg_season(CREDIT_TS)
   #autoplot(box_cox(Month, lambda))
 
 CREDIT_TS %>% 
-  filter(year(Month) >= '1970 Feb') -> CREDIT_TS
+  filter(year(Month) >= "1970 Feb") -> CREDIT_TS
 
-CREDIT_TS %>% 
-  filter(year(Month) <= '2004 Jan') -> TRAINCREDIT
+train <- CREDIT_TS %>% 
+  filter(Month <= yearmonth("2004 Jan"))
+
+holdout <- CREDIT_TS %>% 
+  filter(Month > yearmonth("2004 Jan"))
+
+Model <- train %>% 
+  stretch_tsibble(.init = 48, .step = 24) %>% 
+  model(
+    Naive = NAIVE(credit_in_millions),
+    Drift = RW(credit_in_millions ~ drift()),
+    arima012011 = ARIMA(credit_in_millions ~ pdq(0,1,2) + PDQ(0,1,1)),
+    arima210011 = ARIMA(credit_in_millions ~ pdq(2,1,0) + PDQ(0,1,1)),
+    stepwise = ARIMA(credit_in_millions),
+    ETS = ETS(credit_in_millions),
+    # search = ARIMA(credit_in_millions, stepwise=FALSE)
+    # Neural = NNETAR(credit_in_millions)
+  )
+
+Model %>% 
+  forecast(h = 6) %>% 
+  accuracy(train) %>% 
+  arrange(RMSE)
+
